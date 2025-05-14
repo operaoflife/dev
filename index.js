1ed7cbf76d5216c69d3d4e8a2f5b31496b3ed339
operaoflife <108854859+operaoflife@users.noreply.github.com>
2025-05-14 23:39:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var app = express();
+// A simple HTTP server example
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('{ "response": "welcome to kloudmate" }');
-});
+const requestHandler = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from GitHub Actions CI/CD!');
+};
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+const server = http.createServer(requestHandler);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
-});
-app.listen(process.env.PORT || 5000);
-module.exports = app;
