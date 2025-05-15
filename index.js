e91a5a8b1915b8bb56e1fcce4d52a1b2cda6d817
reema <reemareema75413@gmail.com>
2025-05-15 12:56:44 +0530

    Trigger Dev CI

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-// A simple HTTP server example
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-const requestHandler = (req, res) => {
+http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from GitHub Actions CI/CD without Docker!');
-};
-
-const server = http.createServer(requestHandler);
-server.listen(port, () => {
+  res.end('Hello from CI/CD pipeline!');
+}).listen(port, () => {
   console.log(`Server running on port ${port}`);
-});
-"// trigger Dev CI" 
-"// trigger Dev CI" 
-"// trigger Dev CI" 
-"// trigger Dev CI" 
-"// trigger Dev CI" 
+});"// trigger Dev CI" 
