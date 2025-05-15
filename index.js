3c20b5fa2d34e67506e6e1444ac66cc8630d5f9c
operaoflife <108854859+operaoflife@users.noreply.github.com>
2025-05-15 11:07:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-// A simple HTTP server example
 const http = require('http');
 const port = process.env.PORT || 3000;
-
-const requestHandler = (req, res) => {
+http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from GitHub Actions CI/CD!');
-};
-
-const server = http.createServer(requestHandler);
-server.listen(port, () => {
+  res.end('Hello from CI/CD!');
+}).listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
