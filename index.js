d0f077cb43489658b7f4f8c490d3ca34ecc811ed
operaoflife <108854859+operaoflife@users.noreply.github.com>
2025-05-15 11:12:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from CI/CD!');
+  res.end('Hello from CI/CD pipeline!');
 }).listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
